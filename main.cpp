184f8740a924d56352d5975799ca1af06cd2c218
zhangheng <zhangheng@xust@163.com>
2017-05-20 00:29:56 +0800

    14.39

--- main.cpp
+++ main.cpp
@@ -9,8 +9,11 @@ using std::fstream;
 
 class Equal{
 public:
-    bool operator()(const string &s, size_t k){
-        return s.size()==k;
+    //bool operator()(const string &s, size_t k){
+    //    return s.size()==k;
+    //}
+    bool operator()(const string &s, size_t m, size_t n){
+        return s.size()>m && s.size()<n;
     }
 };
 
@@ -18,8 +21,8 @@ int main()
 {
     fstream in("test1");
     int count = 0;
-    int n;
-    std::cin>>n;
+    int m,n;
+    std::cin>>m>>n;
     Equal obj;
     string s1;
     string s2;
@@ -27,7 +30,7 @@ int main()
         std::getline(in,s1);
         std::istringstream record(s1);
         while(record>>s2){
-            if(obj(s2,n))
+            if(obj(s2,m,n))
                 ++count;
         }
     }
